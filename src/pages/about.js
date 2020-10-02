0aae01f7f899c7f28f50103d83e80211a33ec271
JasonCheng1 <stealthyjason@gmail.com>
2020-10-02 02:02:57 -0400

    updated about me section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,27 +1,24 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import styled from "@emotion/styled"
 
-// const Container = styled.div`
-//   margin: 0 auto;
-//   max-width: 860px;
-//   padding: 0 1.0875rem 1rem;
-//   padding-top: 0;
-// `
 const PageName = () => (
   <Layout>
     <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
 
     <h1>About Me</h1>
     <p>
-      Hi, my name is Jason Cheng and I am a junior studying Computer Science at
-      Rutgers Univeristy- New Brunswick. I have a passion for full stack
-      development and big data. This pass summer, I interned at Optum where I
-      built an employee data analytics web app for the human capital team at
-      UHG. During this internship, I learned a lot of frontend technologies
-      including html, css, js, react, and redux, and also actively learning the
-      latest web development tools.
+      Hi, my name is Jason Cheng, and I am a junior studying Computer Science at
+      Rutgers University-New Brunswick. I have a passion for full-stack
+      development and big data.
+    </p>
+    <p>
+      This past summer, I interned at Optum, where I built an employee data,
+      analytics web app for the human capital team at UHG. During this
+      internship, I learned many front-end technologies, including HTML, CSS,
+      Javascript, React, and Redux. This semester, I am learning how to write
+      back-end server applications and researching the feasibility of an SNN for
+      population estimation using satellite imagery data.
     </p>
     <p>
       When I am not coding, I like to brush up on my mandarin and badminton
